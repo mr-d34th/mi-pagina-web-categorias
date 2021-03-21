5586972dab9f79236408f8ea14475bba3163f7a6
Deyvis <deyvisjaime17@gmail.com>
2021-03-21 01:36:00 -0500

    estilos

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-
 document.getElementById('id-imagen1').addEventListener('click', function() { 
     var x = document.getElementById('alerta');
     if (x.style.display === 'none') {
@@ -8,31 +7,34 @@ document.getElementById('id-imagen1').addEventListener('click', function() {
     }
 })
 
-document.getElementById('id-imagen2').addEventListener('click', function() { 
-    var x = document.getElementById('alerta1');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
+// // document.getElementById('id-imagen2').addEventListener('click', function() { 
+// //     var x = document.getElementById('alerta1');
+// //     if (x.style.display === 'none') {
+// //         x.style.display = 'block';
+// //     } else {
+// //         x.style.display = 'none';
+// //     }
+// // })
+
+// document.getElementById('id-imagen3').addEventListener('click', function() { 
+//     var x = document.getElementById('alerta2');
+//     if (x.style.display === 'none') {
+//         x.style.display = 'block';
+//     } else {
+//         x.style.display = 'none';
+//     }
+// })
+
+// document.getElementById('id-imagen4').addEventListener('click', function() { 
+//     var x = document.getElementById('alerta3');
+//     if (x.style.display === 'none') {
+//         x.style.display = 'block';
+//     } else {
+//         x.style.display = 'none';
+//     }
+// })
+
+const añadir = document.getElementById('añadir');
+añadir.addEventListener('click',()=>{
+    document.querySelector('.producto-img').setAttribute('src','img/1.jpg')
 })
-
-document.getElementById('id-imagen3').addEventListener('click', function() { 
-    var x = document.getElementById('alerta2');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-})
-
-document.getElementById('id-imagen4').addEventListener('click', function() { 
-    var x = document.getElementById('alerta3');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-})
-
-

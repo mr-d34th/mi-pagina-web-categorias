96888863d50b35627ca6b74d33a490d927075e2f
mr-de34th <jorge99bc@hotmail.com>
2021-03-20 19:44:13 -0500

    mi commit de paginas web categorias 1

---
+++ javascript.js
@@ -0,0 +1,38 @@
+
+document.getElementById('id-imagen1').addEventListener('click', function() { 
+    var x = document.getElementById('alerta');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+})
+
+document.getElementById('id-imagen2').addEventListener('click', function() { 
+    var x = document.getElementById('alerta1');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+})
+
+document.getElementById('id-imagen3').addEventListener('click', function() { 
+    var x = document.getElementById('alerta2');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+})
+
+document.getElementById('id-imagen4').addEventListener('click', function() { 
+    var x = document.getElementById('alerta3');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+})
+
+

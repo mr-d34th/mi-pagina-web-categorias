a483d09a7313bc748d3ca683b7959339e6095074
mr-de34th <jorge99bc@hotmail.com>
2021-03-21 19:31:41 -0500

    Agregando carrito

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
-document.getElementById('id-imagen1').addEventListener('click', function() { 
-    var x = document.getElementById('alerta');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-})
+// document.getElementById('id-imagen1').addEventListener('click', function() { 
+//     var x = document.getElementById('alerta');
+//     if (x.style.display === 'none') {
+//         x.style.display = 'block';
+//     } else {
+//         x.style.display = 'none';
+//     }
+// })
 
 // // document.getElementById('id-imagen2').addEventListener('click', function() { 
 // //     var x = document.getElementById('alerta1');
@@ -33,14 +33,95 @@ document.getElementById('id-imagen1').addEventListener('click', function() {
 //         x.style.display = 'none';
 //     }
 // })
-
-const añadir = document.getElementById('añadir');
-añadir.addEventListener('click',(e)=>{
-    e.preventDefault()
-    document.querySelector('.producto-img').setAttribute('src','img/1.jpg')
-})
-
 const close = document.querySelector('.close-modal');
 close.addEventListener('click',()=>{
     document.querySelector('.modal').style.setProperty('display','none')
 })
+
+const general = document.getElementById('todo')
+
+general.addEventListener('click',e=>{
+    if(e.target.classList.contains('image')){
+        document.querySelector('.modal').style.setProperty('display','flex')
+        
+        modal(e.target.parentElement)
+        
+    }
+    e.stopPropagation()
+
+})
+//atributos
+const  modal = (hijo)=>{
+    let image = document.getElementById('imgModal')
+    let precio = document.getElementById('precio')
+    let titulo = document.getElementById('nombre')
+
+    const producto = {
+        nombre: hijo.querySelector('.titulo').textContent,
+        precio: hijo.querySelector('.precio').textContent,
+        imagen:hijo.querySelector('.image').src
+    }
+    precio.value = producto.precio
+    titulo.value = producto.nombre
+    image.setAttribute('src',producto.imagen)
+   
+}
+
+const cesta = {}
+const añadir = document.getElementById('añadir')
+añadir.addEventListener('click',(e)=>{
+    
+    let cantidad = document.querySelector('.cantidad').value
+    // let image = document.getElementById('imgModal')
+    // let precio = document.getElementById('precio')
+    // let titulo = document.getElementById('nombre')
+    
+
+    const producto = {
+        nombre: document.querySelector('.nombre').value,
+        precio: document.querySelector('#precio').value,
+        imagen:document.querySelector('#imgModal').src,
+        cantidad:cantidad
+    }
+    cesta[producto.nombre]= {...producto}
+    let llenado = ''
+    console.log(cesta)
+    Object.values(cesta).forEach(pro =>{
+        llenado += `
+        
+    <div class="carrito">
+    <h2 class="nombre-cesta">${pro.nombre}</h2>
+    <img src="${pro.imagen}" alt="" class="image-cesta">
+    <p class="precio-cesta">${pro.precio}</p>
+    <p class="cantidad">${pro.cantidad}</p>
+   </div>`
+
+       
+       
+    })
+    document.querySelector('.cesta').innerHTML = llenado
+    document.querySelector('.modal').style.setProperty('display','none')
+    
+    
+    
+
+})
+
+
+
+
+//   //FUNCION QUE MUESTRA LOS PRODUCTOS AL TOCAR
+//   const setModal = (objeto) =>{
+//     let nombre = document.getElementById('nombre')
+//     let precio = document.getElementById('precio')
+//     let imageProducto = document.querySelector('.imageProducto')
+//     const producto = {
+//       id:objeto.querySelector('.productoSelecionado').dataset.id,
+//       nombre: objeto.querySelector('.nombreProducto').textContent,
+//       precio:objeto.querySelector('.precioProducto').textContent,
+//       imagen:objeto.querySelector('.productoSelecionado').src,
+//     }
+//     nombre.value= producto.nombre
+//     precio.value=producto.precio
+//     imageProducto.setAttribute('src',producto.imagen)
+//   }
